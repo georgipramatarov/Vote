cd14186603f21dbcdce732f85b7d22b8aac31630
97bee <bernicethomas@icloud.com>
2017-03-01 13:04:16 +0000

    Update and rename banner.php to head.blade.php

--- drive-download-20170301T114909Z-001/head.blade.php
+++ drive-download-20170301T114909Z-001/head.blade.php
@@ -1,7 +1,5 @@
-<!DOCTYPE html>
-<html>
 <head>
 <link rel="stylesheet" href"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <h1 style=" background-color: LightGray"><br/><center>Secure Vote <img src="padlock.png" height="50"></center><br/></h1>
 </head>
-</html>
+
