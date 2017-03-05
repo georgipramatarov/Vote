a961de1f610ffa70e5c19c090bb70d658f5aa252
97bee <bernicethomas@icloud.com>
2017-03-05 21:52:35 +0000

    Update details.blade.php

--- drive-download-20170301T114909Z-001/details.blade.php
+++ drive-download-20170301T114909Z-001/details.blade.php
@@ -3,14 +3,15 @@
  @include('includes.head')
 <body>
 <?php
- 
-  $query = "SELECT * FROM candidates WHERE ID = $_GET[ID]";
+ <?php
+  $con = mysqli_connect("csmysql.cs.cf.ac.uk", "group8.2016", "dafEvUth5", "group8_2016");
+  $query = "SELECT * FROM Candidates WHERE ID = $_GET[	Candidate_ID]";
   $result = mysqli_query($con,$query);
   while($row = mysqli_fetch_assoc($result)){
     echo "<tr>";
-    echo "<td>".$cands['cand_id']."</td>";
-    echo "<td>".$cands['cand_name']."</td>";
-    echo "<td>".$cands['longdescription']."</td>";
+    echo "<td>".$cands['Name']."</td>";
+    echo "<td>".$cands['Polotical_Party']."</td>";
+    echo "<td>".$cands['Info_Long']."</td>";
     echo "<td>";?><img src="<?php echo $cands["cand_img"];?>"height="120" width="120"><?php 
     echo "<td/>";
     echo "<tr/>";
