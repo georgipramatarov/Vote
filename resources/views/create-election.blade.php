0f4eb1e43019341baa1d6259c9d493cf5a3f09b3
97bee <bernicethomas@icloud.com>
2017-03-13 22:14:44 +0000

    Created the create new election from
    The form allows you to enter the election title and the start and end dates, unsure as to what we are doing with adding candidates to the vote ect.

--- resources/views/create-election.blade.php
+++ resources/views/create-election.blade.php
@@ -1,1 +1,49 @@
+<html>
+ @include('includes.head')
+<title>Secure Vote - Administrator</title>
+<body>
+form class="form-horizontal">
+<fieldset>
 
+<!-- Form Name -->
+<legend>New Election</legend>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Election_title">Election Title</label>  
+  <div class="col-md-5">
+  <input id="Election_title" name="Election_title" type="text" placeholder="Election title" class="form-control input-md" required="">
+  <span class="help-block">help text goes here</span>  
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="startdate">Start Date</label>  
+  <div class="col-md-4">
+  <input id="startdate" name="startdate" type="text" placeholder="DD/MM/YYYY" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="enddate">End Date</label>  
+  <div class="col-md-4">
+  <input id="enddate" name="enddate" type="text" placeholder="DD/MM/YYYY" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Submit"></label>
+  <div class="col-md-4">
+    <button id="Submit" name="Submit" class="btn btn-default">Submit</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+</body>
+</HTML>
