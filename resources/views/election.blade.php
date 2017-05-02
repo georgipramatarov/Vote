03c5fd2cff650bcf8b51b042a7c0e41e8c7a6c8a
Tom Wynne-Owen <tomwo123@gmail.com>
2017-05-03 00:42:46 +0100

    Election admin panel

--- resources/views/election.blade.php
+++ resources/views/election.blade.php
@@ -7,14 +7,39 @@
     <div class="col-md-0 ">
         <div class="panel panel-default">
     		<div class="panel-heading" style="background-color:lightgray">Current Elections</div>
-    		<div class="panel-body">
+    		<div class="panel-body">  
 
-    			@foreach ($elections as $elec)
-                  <div class="panel-body col-md-7">{{ $elec->election_name }} </div>
-                  <div class="panel-body col-md-3"> Deadline {{ Carbon\Carbon::parse($elec->close_date)->diffForHumans()}} </div>
-                  <form method="post">
+    		@if (sizeof($elections) > 0)
+    		  <strong>
+    			<div class="panel-body col-md-4">Name</div>
+                <div class="panel-body col-md-3">Starts</div>
+                <div class="panel-body col-md-2">Ends</div>
+                <div class="panel-body col-md-1">Options</div>
 
-    			@endforeach
+              </strong>
+    		@endif
+
+    		@foreach ($elections as $elec)
+                  <div class="panel-body col-md-4">{{ $elec->election_name }} </div>
+                  <div class="panel-body col-md-3">{{ Carbon\Carbon::parse($elec->start_date)->diffForHumans()}} </div>
+                  <div class="panel-body col-md-2"> {{ $elec->close_date }} </div>
+
+                  <!-- buttons -->
+                  <div class="panel-body col-md-2">
+                  <div class="btn-group">
+                  	<!-- Edit -->
+                  	<a href="#" class="btn btn-primary btn-sm">
+          				Edit
+			        </a>
+
+			       <!-- delete -->
+			        <a href="#" class="btn btn-danger btn-sm">
+          				Delete
+			        </a>
+			        </div>
+                  </div>
+
+    		@endforeach
 
     			<div class="col-md-12" style="margin-bottom: 1em; margin-top: 1em; text-align: right">
     				<a class="btn btn-primary" role="button" href="{{ url('admin_home/create-election') }}">Create New</a>
